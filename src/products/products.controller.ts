eb8697bc4ac07e3787a0a6d45c7c0c0f7401e4f5
Palash Mondal <palashm@live.in>
2020-07-23 11:41:48 +0530

    Create update product endpoint

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param, Patch, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put, Delete } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -23,5 +23,16 @@ export class ProductsController {
   @Get(':id')
   getProduct(@Param('id') prodId: string) {
     return this.productsService.getProduct(prodId);
+  }
+
+  @Put(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    this.productsService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+    return { message: 'Product updated successfully.' };
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -25,11 +25,35 @@ export class ProductsService {
     return { ...product };
   }
 
+  updateProduct(productId: string, title: string, desc: string, price: number) {
+    const product = this.findProduct(productId);
+    const index = this.findProductIndex(productId);
+    const updatedProduct = { ...product };
+    if (title) {
+      updatedProduct.title = title;
+    }
+    if (desc) {
+      updatedProduct.description = desc;
+    }
+    if (price) {
+      updatedProduct.price = price;
+    }
+    this.products[index] = updatedProduct;
+  }
+
   private findProduct(id: string): Product {
     const product = this.products.find(prod => prod.id === id);
     if (!product) {
       throw new NotFoundException('Could not find product.');
     }
     return product;
+  }
+
+  private findProductIndex(id: string): number {
+    const productIndex = this.products.findIndex(prod => prod.id === id);
+    if (productIndex < 0) {
+      throw new NotFoundException('Could not find product index.');
+    }
+    return productIndex;
   }
 }
